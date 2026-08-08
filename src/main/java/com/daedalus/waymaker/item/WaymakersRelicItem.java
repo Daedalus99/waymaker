9952a68a28dd79f7e8bac0e2beb9fb2a981682d8
Daedalus99 <locke.harper@gmail.com>
2026-08-08 18:30:11 -0400

    Hold right click to use. Hs animation!

--- src/main/java/com/daedalus/waymaker/item/WaymakersRelicItem.java
+++ src/main/java/com/daedalus/waymaker/item/WaymakersRelicItem.java
@@ -10,8 +10,11 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityTypes;
 import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.portal.TeleportTransition;
 
@@ -22,6 +25,13 @@ public class WaymakersRelicItem extends Item {
 
     @Override
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
+        player.startUsingItem(hand);
+        return InteractionResult.CONSUME;
+    }
+
+    @Override
+    public ItemStack finishUsingItem(final ItemStack stack, final Level level, final LivingEntity entity) {
+        Player player = (Player) entity;
         if (!level.isClientSide()) {
             ServerPlayer p1 = (ServerPlayer) player;
             TeleportTransition player_spawn_location = p1.findRespawnPositionAndUseSpawnBlock(
@@ -39,8 +49,20 @@ public class WaymakersRelicItem extends Item {
             player.teleport(player_spawn_location);
             this.playsound(level, player);
         }
-        return InteractionResult.SUCCESS;
+        return stack;
     }
+
+    @Override
+    public int getUseDuration(ItemStack stack, LivingEntity player){
+        // 2 seconds
+        return 45;
+    }
+
+    @Override
+    public ItemUseAnimation getUseAnimation(ItemStack stack){
+        return ItemUseAnimation.BRUSH;
+    }
+
     private void playsound(Level level, Player player){
         double x = player.getX(), y = player.getY(), z = player.getZ();
         level.playSound(null, x, y, z, SoundEvents.PLAYER_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
